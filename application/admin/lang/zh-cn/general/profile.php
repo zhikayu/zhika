372be26dd24b50d9a73b15b714ec700e77d9b8e5
小和 <496631085@qq.com>
2024-06-17 01:56:09 +0000

    !465 修改字段错误问题 * 修复字段错误

--- application/admin/lang/zh-cn/general/profile.php
+++ application/admin/lang/zh-cn/general/profile.php
@@ -2,7 +2,7 @@
 
 return [
     'Url'                                         => '链接',
-    'Userame'                                     => '用户名',
+    'Username'                                    => '用户名',
     'Createtime'                                  => '操作时间',
     'Click to edit'                               => '点击编辑',
     'Admin log'                                   => '操作日志',
